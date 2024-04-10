3d8599760b3655cb2bb16b2e48bc4d787e8d74c7
Jin G <gao-jin@outlook.com>
2024-04-09 21:51:36 -0400

    tried apis

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 6.8510-Final
+
+## put your API keys in .env_template file, and rename to .env
+
+## Install packagesE
+
+```
+ pip install -r requirements. txt
+ ```
+
+## Generate requirements.txt
+
+```
+pip install pipreqs --user
+python -m  pipreqs.pipreqs ./
+```
--- hume_stream.py
+++ hume_stream.py
@@ -1,7 +1,30 @@
+"""
+Real-time measurement streaming
+
+https://dev.hume.ai/docs/expression-measurement-api/websocket
+
+pip install "hume[stream]"
+"""
+
 import asyncio
 from hume import HumeStreamClient, StreamSocket
 from hume.models.config import LanguageConfig, FaceConfig, ProsodyConfig
 
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HUME_API_KEY = os.getenv("HUME_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+print("HUME_API_KEY=", HUME_API_KEY)
+print("OPENAI_API_KEY=", OPENAI_API_KEY)
+
+"""
+Emotional language from text
+"""
 samples = [
     "Mary had a little lamb,",
     "Its fleece was white as snow."
@@ -10,8 +33,13 @@ samples = [
 ]
 
 
+"""
+Facial expressions from an image
+"""
+
+
 async def main():
-    client = HumeStreamClient("Vz2nqOA7FOxeC6sFosfR3M9AIjX29i4aeJF6ytLfOYxrvAv4")
+    client = HumeStreamClient(HUME_API_KEY)
     config = LanguageConfig()
     async with client.connect([config]) as socket:
         for sample in samples:
@@ -23,8 +51,13 @@ async def main():
 asyncio.run(main())
 
 
+"""
+Speech prosody from an audio or video file
+"""
+
+
 async def main():
-    client = HumeStreamClient("Vz2nqOA7FOxeC6sFosfR3M9AIjX29i4aeJF6ytLfOYxrvAv4")
+    client = HumeStreamClient(HUME_API_KEY)
     config = FaceConfig(identify_faces=True)
 
     async with client.connect([config]) as socket:
